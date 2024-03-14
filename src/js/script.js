04d3d849061d0c11733b12061c35bf55a8eee6ee
byemidev <emilioarevalozuniga@gmail.com>
2024-03-14 02:23:25 +0100

    30%: Making layout

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+/*TODO: we need to make some of content as dinamic content. For example:*/
+/**
+ *  how it works the hover items animation
+ *  how it works the links container when the users push them ...
+ */
